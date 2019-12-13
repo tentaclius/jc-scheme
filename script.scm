12c6078bea3a90fa890f5b27cb530d4fe8af72c7
Anton E <tentaclius@gmail.com>
2019-12-13 12:06:13 +0300

    ...

--- script.scm
+++ script.scm
@@ -7,10 +7,12 @@
 ;
 ;  (* 0.3 (calc carrier)) )
 
-(define saw-osc (make <SawOsc> freq: 440 gain: 0.7))
-(define adsr (make <ADSR> A: 0.1 D: 0.2 S: 0.6 R: 0.7))
+(define saw-osc (make <SawOsc> freq: 440 gain: 0.3))
+(define adsr (make <ADSR> A: 0.01 D: 0.12 S: 0.4 R: 0.7))
 
 (define (f t)
   (if (= 0 (modulo t (from-seconds 4)))
-      (note-on adsr))
+    (note-on adsr))
+  (if (= 0 (modulo t (from-seconds 3)))
+    (note-off adsr))
   (* (value adsr) (value saw-osc)) )
